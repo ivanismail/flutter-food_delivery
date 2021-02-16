c2f61522190d6c30032299ac11b6cda4f8e42698
ivanismail <35797281+ivanismail@users.noreply.github.com>
2021-02-16 09:36:21 +0700

    Update BuildListKeranjang.dart

--- lib/src/ui/widget/keranjang/BuildListKeranjang.dart
+++ lib/src/ui/widget/keranjang/BuildListKeranjang.dart
@@ -1,8 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/src/utility/BaseURL.dart';
 
 Widget BuildListKeranjang(
     {BuildContext context,
     AsyncSnapshot<List> snapshot,
     VoidCallback getHasData}) {
-  return Container();
+  return Container(
+    margin: EdgeInsets.only(
+      bottom: 60.0,
+    ),
+    child: ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: snapshot.data.length,
+      itemBuilder: (BuildContext context, int i) {
+        final data = snapshot.data[i];
+
+        int numQty = int.parse(data.qty);
+
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 10.0,
+            bottom: 5.0,
+            left: 15.0,
+            right: 15.0,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 3.0,
+                    blurRadius: 5.0,
+                  )
+                ],
+                color: Colors.white),
+            child: Row(
+              children: [
+                Container(
+                  height: 90.0,
+                  width: 120.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(BaseURL.baseURLImg + data.gambar),
+                      fit: BoxFit.fill,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15.0),
+                      bottomLeft: Radius.circular(15.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
 }
